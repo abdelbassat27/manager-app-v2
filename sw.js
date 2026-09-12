49d2ade5ce274596f60b0de09747f940f89a4d88
abdelbassat <abdoukhadouma10@gmail.com>
2026-09-12 19:28:00 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE = 'manager-erp-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/app.css',
+  './js/db.js',
+  './js/auth.js',
+  './js/ui.js',
+  './js/app.js',
+  './manifest.webmanifest',
+  './icons/icon.svg',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))),
+    ).then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    caches.match(req).then((cached) => {
+      const fetched = fetch(req)
+        .then((res) => {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {});
+          return res;
+        })
+        .catch(() => cached);
+      return cached || fetched;
+    }),
+  );
+});
